2383881dc1ca91fdcf356356a90568f69e15decc
Tim <timwagner997@gmail.com>
2023-11-01 13:04:53 +0100

    added enhanced mediapipe program / refactor

---
+++ Enhanced MediaPipe Example/main.py
@@ -0,0 +1,39 @@
+import mediapipe as mp
+from mediapipe.tasks import python
+import cv2
+from mediapipe.tasks.python import vision
+
+BaseOptions = mp.tasks.BaseOptions
+GestureRecognizer = mp.tasks.vision.GestureRecognizer
+GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+
+# Create a gesture recognizer instance with the live stream mode:
+def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
+    print('gesture recognition result: {}'.format(result))
+
+options = GestureRecognizerOptions(
+    base_options=BaseOptions(model_asset_path='gesture_recognizer.task'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
+
+cap = cv2.VideoCapture(0)
+
+frame_timestamp_ms = 0
+
+with GestureRecognizer.create_from_options(options) as recognizer:
+    while True:
+        ret, frame = cap.read()
+
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+
+        recognizer.recognize_async(mp_image, frame_timestamp_ms)
+
+        cv2.imshow("frame", frame)
+
+        frame_timestamp_ms += 1
+
+        if cv2.waitKey(1) == ord('q'):
+            break
