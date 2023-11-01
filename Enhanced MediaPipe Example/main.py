cf150134a3f3348595f5d69c79fd0c3844de9807
Tim <timwagner997@gmail.com>
2023-11-01 14:11:01 +0100

    added text drawing capability

--- Enhanced MediaPipe Example/main.py
+++ Enhanced MediaPipe Example/main.py
@@ -9,10 +9,16 @@ GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
 GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
+showed_text = ''
+
 
 # Create a gesture recognizer instance with the live stream mode:
 def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
-    print(result.gestures)
+    global showed_text
+
+    for gesture in result.gestures:
+        showed_text = str(gesture[0].category_name)
+
 
 options = GestureRecognizerOptions(
     base_options=BaseOptions(model_asset_path='gesture_recognizer.task'),
@@ -23,9 +29,10 @@ options = GestureRecognizerOptions(
 
 cap = cv2.VideoCapture(0)
 
-frame_timestamp_ms = 0
 
-with GestureRecognizer.create_from_options(options) as recognizer:
+def process_webcam_video():
+    timestamp = 0
+
     while True:
         ret, frame = cap.read()
 
@@ -35,13 +42,20 @@ with GestureRecognizer.create_from_options(options) as recognizer:
 
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
 
-        recognizer.recognize_async(mp_image, frame_timestamp_ms)
+        recognizer.recognize_async(mp_image, timestamp)
 
-        cv2.imshow("frame", frame)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(frame, showed_text, (10, 100), font, 5, (255, 255, 255), 2, cv2.LINE_AA)
 
-        frame_timestamp_ms += 1
+        cv2.imshow('image', frame)
+
+        timestamp += 1
 
         if cv2.waitKey(1) == ord('q'):
             break
+
+
+with GestureRecognizer.create_from_options(options) as recognizer:
+    process_webcam_video()
 
 cap.release()
