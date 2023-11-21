d80d5bfde96f54692040bc4f5225bf1db9f3d082
Tim <timwagner997@gmail.com>
2023-11-21 21:09:23 +0100

    print text of the gesture on the hand (centred)

--- Final Program/combination/visualization.py
+++ Final Program/combination/visualization.py
@@ -10,15 +10,29 @@ def add_visualization(numpy_image, json):
     data = json_parser.loads(json)
 
     for gesture in data['gestures']:
-        abs_pos_x = int(gesture['pos_x'] * width)
-        abs_pos_y = int(gesture['pos_y'] * height)
+        abs_pos_x = gesture['pos_x'] * width
+        abs_pos_y = gesture['pos_y'] * height
 
-        cv2.circle(
-            img=image_with_visualization,
-            center=(abs_pos_x, abs_pos_y),
-            radius=20,
-            color=(0, 255, 255),
-            thickness=-1
+        text = gesture['name']
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 2
+        thickness = 5
+        text_color = (0, 255, 0)
+
+        text_width, text_height = cv2.getTextSize(text, font, font_scale, thickness)[0]
+
+        text_pos_x = int(abs_pos_x - text_width / 2)
+        text_pos_y = int(abs_pos_y + text_height)
+
+        cv2.putText(
+            image_with_visualization,
+            text,
+            (text_pos_x, text_pos_y),
+            font,
+            font_scale,
+            text_color,
+            thickness
         )
 
     return image_with_visualization
