547ccaf7dfa1fc3f4b542630c49c5af9039070ed
Tim <timwagner997@gmail.com>
2023-11-22 09:34:24 +0100

    refactor

--- Final Program/combination/visualization.py
+++ Final Program/combination/visualization.py
@@ -11,7 +11,7 @@ text_color = (0, 255, 0)
 def add_visualization(numpy_image, json):
     image_with_visualization = numpy_image.copy()
 
-    height, width, channels = numpy_image.shape
+    screen_height, screen_width, channels = numpy_image.shape
 
     data = json_parser.loads(json)
 
@@ -19,8 +19,8 @@ def add_visualization(numpy_image, json):
     for gesture in data['gestures']:
         text = gesture['name']
 
-        abs_pos_x = gesture['pos_x'] * width
-        abs_pos_y = gesture['pos_y'] * height
+        abs_pos_x = gesture['pos_x'] * screen_width
+        abs_pos_y = gesture['pos_y'] * screen_height
 
         text_width, text_height = cv2.getTextSize(text, font, font_scale, thickness)[0]
 
@@ -43,8 +43,8 @@ def add_visualization(numpy_image, json):
 
     text_width, text_height = cv2.getTextSize(text, font, font_scale, thickness)[0]
 
-    text_pos_x = int(width - text_width)
-    text_pos_y = int(height - text_height / 2)
+    text_pos_x = int(screen_width - text_width)
+    text_pos_y = int(screen_height - text_height / 2)
 
     cv2.putText(
         image_with_visualization,
