ecf0a9540e4a8526c0e67ff69cd4386a8cd786a2
Tim <timwagner997@gmail.com>
2023-11-22 09:33:21 +0100

    added fingersAmount to screen

--- Final Program/combination/visualization.py
+++ Final Program/combination/visualization.py
@@ -2,6 +2,12 @@ import cv2
 import json as json_parser
 
 
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scale = 2
+thickness = 5
+text_color = (0, 255, 0)
+
+
 def add_visualization(numpy_image, json):
     image_with_visualization = numpy_image.copy()
 
@@ -9,16 +15,12 @@ def add_visualization(numpy_image, json):
 
     data = json_parser.loads(json)
 
+    # draw gesture names on the image
     for gesture in data['gestures']:
         text = gesture['name']
 
         abs_pos_x = gesture['pos_x'] * width
         abs_pos_y = gesture['pos_y'] * height
-
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        font_scale = 2
-        thickness = 5
-        text_color = (0, 255, 0)
 
         text_width, text_height = cv2.getTextSize(text, font, font_scale, thickness)[0]
 
@@ -34,5 +36,24 @@ def add_visualization(numpy_image, json):
             text_color,
             thickness
         )
+
+    # draw fingers amount on the image
+    fingers_amount = data['totalFingersAmount']
+    text = f'{fingers_amount} Fingers raised'
+
+    text_width, text_height = cv2.getTextSize(text, font, font_scale, thickness)[0]
+
+    text_pos_x = int(width - text_width)
+    text_pos_y = int(height - text_height / 2)
+
+    cv2.putText(
+        image_with_visualization,
+        text,
+        (text_pos_x, text_pos_y),
+        font,
+        font_scale,
+        text_color,
+        thickness
+    )
 
     return image_with_visualization
